b572ed61a12f091d715b745a08b5868e7da4c4d9
sohei.kakihara <souhei_kakihara@simple-inc.com>
2025-04-12 16:37:32 +0900

    変更

---
+++ server_api/app/Http/Controllers/API/ProjectController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+        return response()->json($projects);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'due_date' => 'nullable|date',
+        ]);
+
+        $project = Project::create($validated);
+        return response()->json($project, 201);
+    }
+
+    public function show(Project $project)
+    {
+        return response()->json($project);
+    }
+
+    public function update(Request $request, Project $project)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'due_date' => 'nullable|date',
+        ]);
+
+        $project->update($validated);
+        return response()->json($project);
+    }
+
+    public function destroy(Project $project)
+    {
+        $project->delete();
+        return response()->json(null, 204);
+    }
+}
---
+++ server_api/routes/api.php
@@ -0,0 +1,4 @@
+<?php
+use App\Http\Controllers\API\ProjectController;
+
+Route::apiResource('projects', ProjectController::class);
